3ba9fc6d65d7401415cecd0f83f76196e4cae873
idmaksim <i@dmaksim.ru>
2024-07-27 20:48:18 +0300

    add comments to views functions

--- src/plans/views.py
+++ src/plans/views.py
@@ -24,11 +24,14 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model=PlanGetSchema, status_code=status.HTTP_200_OK)
+@router.get('/{id}', response_model=PlanGetSchema, status_code=status.HTTP_200_OK)
 async def get_one(
     id: int,
     service: PlanService = Depends(get_plan_service)
 ):
+    """
+    Получение плана-графика по id.
+    """
     plan = await service.get_by_id(id)
     return plan
 
@@ -38,23 +41,32 @@ async def create_one(
     plan: PlanCreateSchema,
     service: PlanService = Depends(get_plan_service)
 ):
+    """
+    Добавление плана-графика.
+    """
     new_plan = await service.create(plan)
     return new_plan
 
 
-@router.delete('/', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_one(
     id: int,
     service: PlanService = Depends(get_plan_service)
 ):
+    """
+    Удаление плана графика по id.
+    """
     await service.delete_by_id(id)
 
 
-@router.get('/all', status_code=status.HTTP_200_OK)
+@router.get('', status_code=status.HTTP_200_OK)
 async def get_all(
     limit: int = 10,
     offset: int = 0,
     service: PlanService = Depends(get_plan_service)
 ):
+    """
+    Получение всех планов-графиков с лимитом (limit) и сдвигом (offset)
+    """
     plans = await service.get_all(limit, offset)
     return plans
--- src/purchases/views.py
+++ src/purchases/views.py
@@ -23,56 +23,74 @@ router = APIRouter(
 )
 
 
-@router.get('/', response_model=PurchaseGetSchema, status_code=status.HTTP_200_OK)
+@router.get('/{id}', response_model=PurchaseGetSchema, status_code=status.HTTP_200_OK)
 async def get_one(
     id: int,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Получение закупки по id.
+    """
     item = await service.get_by_id(id)
     return item
 
 
-@router.get('/status', status_code=status.HTTP_200_OK)
+@router.get('/{id}/status', status_code=status.HTTP_200_OK)
 async def get_status(
     id: int,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Получение статуса закупки по id.
+    """
     status = await service.get_status_by_id(id)
     return {'status': status}
 
 
-@router.put('/update_status', status_code=status.HTTP_200_OK)
+@router.put('/{id}/status', status_code=status.HTTP_200_OK)
 async def update_status(
     id: int,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Запуск обновления статуса закупки по id.
+    """
     await service.update_status(id)
     return {'message': 'start updating status'}
 
 
-@router.put('/', status_code=status.HTTP_200_OK)
+@router.post('/{plan_id}/', status_code=status.HTTP_200_OK)
 async def create_one(
     plan_id: int,
     purchase: PurchaseCreateSchema,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Добавление закупки в план-график по id плана.
+    """
     purchase = await service.add(purchase, plan_id)
     return purchase
 
 
-@router.delete('/', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_one(
     id: int,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Удаление закупки по id из плана-графика и таблицы с закупками
+    """
     await service.delete(id)
 
 
-@router.get('/all', status_code=status.HTTP_200_OK)
+@router.get('', status_code=status.HTTP_200_OK)
 async def get_all(
     limit: int = 10,
     offset: int = 0,
     service: PurchaseService = Depends(get_purchases_service),
 ):
+    """
+    Получение всех закупок с лимитом (limit) и сдвигом (offset).
+    """
     items = await service.get_all(limit=limit, offset=offset)
     return items
